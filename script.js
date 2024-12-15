4a7bff1b33acc960ea35617c06a62a38bf2ea074
Abdullah khan <akhan283785@gmail.com>
2024-12-15 23:47:40 +0530

    try

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
 // write js code here if required
+let timer = document.querySelector("#timer");
+let time = new Date();
+
+timer.innerHTML = `${time.getDay()}/${time.getMonth()}/${time.getFullYear()}, ${time.getHours()}:${time.getMinutes()}:${time.getSeconds()}`;
